425bd5b7edcfa8b0de223a1186375d0e08a63496
Vinicius Cardoso <vinny.cardoso96@gmail.com>
2020-02-16 07:11:59 -0300

    Load and transform images

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -21,3 +21,18 @@ fps = 30
 CLOCK = pg.time.Clock()
 screen = pg.display.set_mode((width, height+100),0,32)
 pg.display.set_caption('Tic Tac Toe')
+
+#loading the images
+opening = pg.image.load('tic tac opening.png')
+x_img = pg.image.load('x.png')
+o_img = pg.image.load('o.png')
+
+#resizing images
+x_img = pg.transform.scale(x_img, (80,80))
+o_img = pg.transform.scale(o_img, (80,80))
+opening = pg.transform.scale(opening, (width, height+100))
+def game_opening():
+    screen.blit(opening, (0,0))
+    pg.display.update()
+    time.sleep(1)
+    screen.fill(white)
